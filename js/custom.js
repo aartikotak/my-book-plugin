11c48776b3bb3073fa2e485b5fff3259c96ac2c3
aartikotak <96609912+aartikotak@users.noreply.github.com>
2021-12-24 12:49:04 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+
+	$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#example tfoot th').each( function () {
+        var title = $('#example thead th').eq( $(this).index() ).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+ 
+    // DataTable
+    var table = $('#example').DataTable();
+     
+    // Apply the filter
+    $("#example tfoot input").on( 'keyup change', function () {
+        table
+            .column( $(this).parent().index()+':visible' )
+            .search( this.value )
+            .draw();
+    } );
+} );
